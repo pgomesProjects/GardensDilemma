7dac7e7863e757abaf9ed1a682e84d3fe4f48ef2
Peter Gomes <pgomes3@student.fitchburgstate.edu>
2022-02-08 00:18:21 -0500

    Enemy Event Created

---
+++ Assets/Scripts/BossEventController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEventController : MonoBehaviour
+{
+    public GameObject bossSprite;
+    public float moveTime;
+    public float yPos;
+
+    public Vector2 startPos;
+    public Vector2 endPos;
+
+    private bool bossActive = false;
+    private float timeStartedMove;
+
+    void Update()
+    {
+        if (bossActive)
+        {
+            bossSprite.transform.localPosition = UseLerp(startPos, endPos, timeStartedMove, moveTime);
+        }    
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!bossActive)
+        {
+            bossActive = true;
+            timeStartedMove = Time.time;
+        }
+    }//end of OnTriggerEnter2D
+    public Vector3 UseLerp(Vector3 start, Vector3 end, float timeStarted, float lerpTime = 1)
+    {
+        float timeSinceStarted = Time.time - timeStartedMove;
+
+        float percentage = timeSinceStarted / lerpTime;
+        start.z = 1;
+        end.z = 1;
+        return Vector3.Lerp(start, end, percentage);
+
+    }//end of UseLerp
+}
