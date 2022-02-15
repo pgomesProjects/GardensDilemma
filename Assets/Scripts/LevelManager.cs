22eb47b1925b4130bccc4e7b432431d09351679d
Peter Gomes <pgomes3@student.fitchburgstate.edu>
2022-02-15 04:32:13 -0500

    Added Background Layers To Scene and Background Music

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -14,6 +14,7 @@ public class LevelManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        FindObjectOfType<AudioManager>().Play("LevelBGM", 1.0f);
         isPaused = false;
         pauseMenu.SetActive(false);
         isGameActive = true;
